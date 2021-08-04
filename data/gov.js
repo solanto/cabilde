5ca4727c2d47e8983052547dda46d3120c515036
Andrew Solanto <atsolanto@gmail.com>
2021-08-04 16:44:20 -0400

    datos de iniciativas

--- data/gov.js
+++ data/gov.js
@@ -1,4 +1,12 @@
 import fetch from "sync-fetch"
+import renderName from "../lib/render-name"
 
 // TODO: use github api
-export const representantes = fetch("https://raw.githubusercontent.com/solanto/cabildummy-datos/master/diputados.json").json()
+export const
+    representantes = fetch("https://raw.githubusercontent.com/solanto/cabildummy-datos/master/diputados.json")
+        .json()
+        .sort(({ nombre }) => renderName(nombre)),
+    iniciativas = fetch("https://raw.githubusercontent.com/solanto/cabildummy-datos/master/iniciativas.json")
+        .json()
+        .sort(({ fecha: { iniciada } }) => new Date(iniciada))
+        .reverse()
